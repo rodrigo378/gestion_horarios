69ab49dbf0068e08775ee741f31710c5de9ed244
VSinche <115099082+Vitocho22@users.noreply.github.com>
2025-02-18 18:29:42 -0500

    Registro al 40%

--- src/app/pages/registro-docentes/registro-docentes.component.ts
+++ src/app/pages/registro-docentes/registro-docentes.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './registro-docentes.component.css'
 })
 export class RegistroDocentesComponent {
+  currentStep =  1;
+  totalSteps = 10;
 
+  nextStep() {
+    if (this.currentStep < this.totalSteps) {
+      this.currentStep++;
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
 }
