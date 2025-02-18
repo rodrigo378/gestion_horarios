db12ae31afbbabc1935f09379603c4ef666e8b3f
VSinche <115099082+Vitocho22@users.noreply.github.com>
2025-02-18 15:38:51 -0500

    Commit

---
+++ src/app/services/auth-google.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGoogleService {
+
+  constructor(private oauthService: OAuthService) {
+    this.initLogin();
+  }
+
+  initLogin() {
+    const config: AuthConfig = {
+      issuer: 'https://accounts.google.com',
+      strictDiscoveryDocumentValidation: false,
+      clientId: 'YOUR_CLIENT_ID',
+      redirectUri: window.location.origin + '/main',
+      scope: 'openid profile email',
+    }
+
+    this.oauthService.configure(config);
+    this.oauthService.setupAutomaticSilentRefresh();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+}
