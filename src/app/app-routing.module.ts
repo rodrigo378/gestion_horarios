20af9220773f86fb9c392bfdc6ef980a78fbba51
RODRIGO PALOMINO CASTRO <rodrigo.palomino@umared.com>
2025-02-21 16:36:09 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './common/layout/layout.component';
 import { RegistroDocentesComponent } from './pages/registro-docentes/registro-docentes.component';
+import { DocentesComponent } from './pages/docentes/docentes.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -11,14 +12,19 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'registrodocentes' },
-      { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-      { path: 'registrodocentes', component: RegistroDocentesComponent }
-    ]
-  }
+      {
+        path: 'welcome',
+        loadChildren: () =>
+          import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+      },
+      { path: 'registrodocentes', component: RegistroDocentesComponent },
+      { path: 'docentes', component: DocentesComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
