8c3a0855e723ac694a6e0e9041e5b7ea62db8118
RODRIGO PALOMINO CASTRO <rodrigo.palomino@umared.com>
2025-02-25 18:05:20 -0500

    stage

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from '../../interfaces/user';
+import { User } from '../../interfaces/User';
 import { HttpErrorResponse } from '@angular/common/http';
 import { AuthGoogleService } from '../../services/auth-google.service';
 import { ActivatedRoute, Router } from '@angular/router';
