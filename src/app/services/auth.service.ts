6caec37177452824592e016fdaa8fb94b4e9a435
RODRIGO PALOMINO CASTRO <rodrigo.palomino@umared.com>
2025-02-25 17:38:24 -0500

    arreglandi importacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environment/environment';
-import { User } from '../interfaces/user';
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/User';
 
 @Injectable({
   providedIn: 'root',
