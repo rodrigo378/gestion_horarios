aeceb2fe492037b83ee9215349412f57263cf164
RODRIGO PALOMINO CASTRO <rodrigo.palomino@umared.com>
2025-03-27 16:13:16 -0500

    transversales

--- src/app/pages/ver-cursos/ver-cursos.component.ts
+++ src/app/pages/ver-cursos/ver-cursos.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HorarioService } from '../../services/horario.service';
-// import { Horario } from '../../interfaces/Horario';
 import { Especialidad } from '../../interfaces/Curso';
 import { CursoService } from '../../services/curso.service';
+import { Horario } from '../../interfaces/Horario';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-ver-cursos',
@@ -11,8 +13,8 @@ import { CursoService } from '../../services/curso.service';
   styleUrl: './ver-cursos.component.css',
 })
 export class VerCursosComponent implements OnInit {
-  // horarios: Horario[] = [];
-  // horario!: Horario;
+  horarios: Horario[] = [];
+  horario!: Horario;
   especialidades: Especialidad[] = [];
   especialidadesModal: Especialidad[] = [];
   cursosFiltrados: any[] = [];
@@ -34,42 +36,43 @@ export class VerCursosComponent implements OnInit {
 
   constructor(
     private horarioService: HorarioService,
-    private cursoService: CursoService
+    private cursoService: CursoService,
+    private alertService: AlertService
   ) {}
 
   ngOnInit(): void {}
 
   getHorarios() {
     this.horarioService
-      // .getHorarios(
-      //   this.selectModalidad,
-      //   Number(this.selectPeriodo),
-      //   this.selectFacultadad,
-      //   this.selectEspecialidad,
-      //   Number(this.selectPlan)
-      // )
-      // .subscribe((data) => {
-      //   this.horarios = data;
-      //   console.log('cursos tabla');
-      // });
+      .getHorarios(
+        this.selectModalidad,
+        Number(this.selectPeriodo),
+        this.selectFacultadad,
+        this.selectEspecialidad,
+        Number(this.selectPlan)
+      )
+      .subscribe((data) => {
+        this.horarios = data;
+        console.log('cursos tabla => ', this.horarios);
+      });
   }
 
-  // getHorariosFiltrados() {
-  //   console.log('filtros => ', this.filtros);
+  getHorariosFiltrados() {
+    console.log('filtros => ', this.filtros);
 
-  //   this.horarioService
-  //     .getHorarios(
-  //       this.filtros.c_codmod,
-  //       undefined,
-  //       this.filtros.c_codfac,
-  //       this.filtros.c_codesp,
-  //       Number(this.filtros.c_codpla)
-  //     )
-  //     .subscribe((data) => {
-  //       this.cursosFiltrados = data;
-  //       console.log('cursos modal => ', data);
-  //     });
-  // }
+    this.horarioService
+      .getHorarios(
+        this.filtros.c_codmod,
+        undefined,
+        this.filtros.c_codfac,
+        this.filtros.c_codesp,
+        Number(this.filtros.c_codpla)
+      )
+      .subscribe((data) => {
+        this.cursosFiltrados = data;
+        console.log('cursos modal => ', data);
+      });
+  }
 
   changeSelectFacultad() {
     this.cursoService.getEspecialidades().subscribe((data) => {
@@ -95,12 +98,32 @@ export class VerCursosComponent implements OnInit {
     this.getHorarios();
   }
 
-  // clickMas(horario: Horario) {
-  //   console.log('horario => ', horario);
-  //   this.mostrarModalCrear = true;
-  // }
+  clickMas(horario: Horario) {
+    console.log('horario => ', horario);
+    this.horario = horario;
+    this.mostrarModalCrear = true;
+  }
 
-  // clickBuscarCursosModal() {
-  //   this.getHorariosFiltrados();
-  // }
+  clickBuscarCursosModal() {
+    this.getHorariosFiltrados();
+  }
+
+  clickMasCursoTransversal(hijo_id: number) {
+    console.log('padre_id => ', this.horario.id);
+    console.log('hijo_id => ', hijo_id);
+
+    this.horarioService
+      .asociarHorarioTransversal(Number(this.horario.id), hijo_id)
+      .subscribe({
+        next: (res: any) => {
+          console.log(res);
+          this.alertService.success('Se crea el curso transversal');
+          this.getHorariosFiltrados();
+        },
+        error: (err: HttpErrorResponse) => {
+          this.alertService.error(err.error.message);
+          console.log(err);
+        },
+      });
+  }
 }
