4f9dfd0336a9da5c248240d55b1d46f41ada6de2
VictorSinche <victor.sinche@uma.edu.pe>
2025-04-14 18:16:20 -0500

    commit

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
-<<<<<<< HEAD
-  production: false,
-  // api: 'http://161.97.115.144:7001', // 🔹 Corrección: La clave debe ser `api`, no `apiUrl`
-  api: 'http://localhost:3000', // 🔹 Corrección: La clave debe ser `api`, no `apiUrl`
-=======
   production: true,
   api: 'http://161.97.115.144:7001', // 🔹 Corrección: La clave debe ser `api`, no `apiUrl`
   // api: 'http://localhost:3000', // 🔹 Corrección: La clave debe ser `api`, no `apiUrl`
->>>>>>> 3e58733ccdb0262ec1c09b00f706687556e575d9
 };
